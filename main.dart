033ae82ebbaf0215948652a789c85b8e6dfdafab
Sachin Sharma <148564606+SachinDevX@users.noreply.github.com>
2024-11-07 08:30:36 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,9 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:messenger/firebase_options.dart';
+import 'package:messenger/services_or_auth/authService.dart';
+import 'package:messenger/services_or_auth/auth_gate.dart';
 import 'package:messenger/services_or_auth/login_or_Register.dart';
+import 'package:provider/provider.dart';
 
 
-void main(){
-  runApp(const MyApp());
+void main() async{
+  runApp(
+      ChangeNotifierProvider(
+          create:(context) => AuthService(),
+        child: const MyApp(),
+      )
+  );
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 }
 
 class MyApp extends StatelessWidget {
@@ -13,8 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginOrRegister(),
+      home: AuthGate(),
     );
   }
 }
-
