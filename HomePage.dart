01d5149903e99d9e799986a36674b377702d4069
Sachin Sharma <148564606+SachinDevX@users.noreply.github.com>
2024-11-08 17:48:30 +0530

    Update HomePage.dart

--- HomePage.dart
+++ HomePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:messenger/services_or_auth/authService.dart';
+import 'package:provider/provider.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -8,10 +10,25 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  //sign user out
+  void signOut(){
+    //get auth Service
+    final authService = Provider.of<AuthService>(context, listen: false);
+    authService.signOut();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home')
+      appBar: AppBar(
+          title: Text('Home'),
+        backgroundColor: Colors.black12,
+        actions: [
+          //sign out button
+          IconButton(
+              onPressed: signOut,
+              icon: const Icon(Icons.logout),
+          )
+        ],
       ),
     );
   }
