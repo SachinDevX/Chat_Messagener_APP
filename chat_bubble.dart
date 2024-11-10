6a94382d791f6d2ee10d4f46348c209eab5813db
Sachin Sharma <148564606+SachinDevX@users.noreply.github.com>
2024-11-10 10:02:50 +0530

    Update chat_bubble.dart

--- chat_bubble.dart
+++ chat_bubble.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
+
 class ChatBubble extends StatelessWidget {
   final String message;
-  const ChatBubble({super.key,
-  required this.message,});
+  final bool isSender; // True if the message is from the sender, false if from the receiver.
+
+  const ChatBubble({
+    super.key,
+    required this.message,
+    required this.isSender, // Add this to determine sender or receiver
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(12),
+      margin: EdgeInsets.only(
+        bottom: 8,
+        left: isSender ? 50 : 10,
+        right: isSender ? 10 : 50,
+      ), // Adjust margin based on sender or receiver
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: Colors.blue,
+        borderRadius: BorderRadius.circular(12),
+        color: isSender ? Colors.blue : Colors.grey, // Different colors for sender and receiver
       ),
-      child: Text(message,
-      style: const TextStyle(fontSize:16, color: Colors.white ),),
+      child: Text(
+        message,
+        style: const TextStyle(fontSize: 16, color: Colors.white),
+      ),
     );
   }
 }
